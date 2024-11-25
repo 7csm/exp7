9d2ea84e523ef71b8dd9f0fc29e25630297292d4
7csm <bpawan3370@gmail.com>
2024-11-25 17:25:20 +0530

    commit

--- exp7/src/app/app-routing.module.ts
+++ exp7/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { AboutComponent } from './about/about.component';
-const routes: Routes = [{
-path:"profile",component:ProfileComponent
-},
-{
-path:"about",component:AboutComponent
-}];
+ 
+
 
 @NgModule({
-imports: [RouterModule.forRoot(routes)],
+imports: [],
 exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- exp7/src/app/app.module.ts
+++ exp7/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AboutComponent } from './about/about.component';
+ 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -13,8 +12,7 @@ import {MatButtonModule} from '@angular/material/button';
 @NgModule({
 declarations: [
 AppComponent,
-ProfileComponent,
-AboutComponent
+ 
 ],
 imports: [
 BrowserModule,
